357ef82f024551385a88a89150b8cabe41e34006
InfluenceFunctional <mjakilgour@gmail.com>
2023-08-01 16:50:55 -0400

    :> test for benzamide defect functionality

--- xyz_to_lammps.py
+++ xyz_to_lammps.py
@@ -12,11 +12,11 @@ from distutils.dir_util import copy_tree
 from run_script import create_xyz_and_run_lammps
 
 '''set head directory'''
-head_dir = r'/home/mk8347/scratch/molecule_clusters/benzamide_test'
-#head_dir = r'C:\Users\mikem\crystals\clusters\cluster_structures/battery_11'
+head_dir = r'/home/mk8347/scratch/molecule_clusters/benzamide_test2'
+# head_dir = r'C:\Users\mikem\crystals\clusters\cluster_structures/battery_11'
 
 crystals_path = r'/scratch/mk8347/molecule_clusters/CrystalStructures/'  #
-#crystals_path = r'C:\Users\mikem\crystals\clusters\Leslie\CrystalStructures/'  #
+# crystals_path = r'C:\Users\mikem\crystals\clusters\Leslie\CrystalStructures/'  #
 
 if not os.path.exists(head_dir):
     os.mkdir(head_dir)
@@ -26,9 +26,9 @@ if not os.path.exists('common'):
     copy_tree('../common', './common/')
 
 cluster_sizes = [[4, 4, 4]]
-temperatures = [200]
-crystal_structures = ["NICOAM13"]
-defect_rates = [0, 0.25, 0.5, 0.75, 1]
+temperatures = [250, 300, 350]
+crystal_structures = ["NICOAM13", "NICOAM17"]
+defect_rates = [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.5]
 
 n_runs = len(cluster_sizes) * len(temperatures) * len(crystal_structures) * len(defect_rates)
 run_nums = list(np.arange(1, n_runs + 1))
@@ -50,8 +50,8 @@ for i in range(len(cluster_sizes)):
 for run_num, size, temp, crystal, defect in zip(run_nums, size_list, temp_list, crystal_list, defect_list):
     create_xyz_and_run_lammps(head_dir, run_num, crystals_path,
                               cluster_size=size,
-                              print_steps=100,
-                              run_time=int(1e5),
+                              print_steps=1000,
+                              run_time=int(1e7),
                               integrator='nosehoover',
                               box_type='p',
                               bulk_crystal=False,
