72c796e2236013c3c34f0f0f5b849b7fd58b5920
InfluenceFunctional <mjakilgour@gmail.com>
2023-09-05 10:18:44 -0400

    :> bound & sphere tests

---
+++ configs/dev5.py
@@ -0,0 +1,25 @@
+'''run configs to loop over - will take all combinations of the below lists - grows combinatorially!!'''
+
+batch_config = {
+    # loop-overable (must be a list)
+    'cluster_size': [[10, 10, 10]],
+    'temperature': [100, 500],
+    'structure_identifier': ["NICOAM13"], #, "NICOAM17"],
+    'defect_rate': [0],
+    'gap_rate': [0],
+    'scramble_rate': [0],
+    'seed': [1],
+    'damping': [str(100.0)],
+    'max_sphere_radius': [10, 20],
+
+    # static items - DO NOT SET AS LIST
+    'cluster_type': 'spherical',
+    'run_time': int(1e4),
+    'box_type': 'p',
+    'integrator': 'nosehoover',
+    'print_steps': int(1e2),
+    'min_inter_cluster_distance': 100,
+    'bulk_crystal': False,
+    'machine': 'cluster',
+    'run_name': 'dev5',
+}
