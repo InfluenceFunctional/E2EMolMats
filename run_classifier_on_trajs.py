58e65766df7b7b8b24484fadd10c9f9766d7c89b
InfluenceFunctional <mjakilgour@gmail.com>
2023-09-20 17:36:43 -0400

    :> hopefully new CNN clustering ready
    :> test config

--- run_classifier_on_trajs.py
+++ run_classifier_on_trajs.py
@@ -7,7 +7,6 @@ run_path = '/scratch/mk8347/molecule_clusters/dev10/'
 os.chdir(run_path)
 
 run_dirs = os.listdir()
-run_dirs = [dir for dir in run_dirs if dir != 'common']
 
 os.system('module load intel/19.1.2')
 os.system('module load openmpi/intel/4.1.1')
@@ -19,6 +18,10 @@ copyfile('example/INPUT_nico_cluster2.dat', 'INPUT.dat')
 os.system('make')
 
 for dir_path in run_dirs:
-    os.chdir(dir_path)
-    os.system('../sa traj.dump ../lammps_vec ../INPUT.dat')
-    os.chdir('../')
+    try:
+        print(int(dir_path))
+        os.chdir(dir_path)
+        os.system('../sa traj.dump ../lammps_vec ../INPUT.dat')
+        os.chdir('../')
+    except:
+        pass
