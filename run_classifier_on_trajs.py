5d545a19df231b83f7dcdfe2af9a16ae1f1b5ec7
InfluenceFunctional <mjakilgour@gmail.com>
2023-09-25 13:31:55 -0400

    :> hopefully new CNN clustering ready
    :> test config

--- run_classifier_on_trajs.py
+++ run_classifier_on_trajs.py
@@ -2,23 +2,20 @@ import os
 from distutils.dir_util import copy_tree
 from shutil import copyfile
 from time import sleep
+from random import shuffle
 
 classifier_source_path = '/scratch/mk8347/daisuke_classifier/molvec_lib/'
-run_path = '/scratch/mk8347/molecule_clusters/dev10/'
+run_path = '/scratch/mk8347/molecule_clusters/defect_clusters_5/'
 os.chdir(run_path)
 
 run_dirs = os.listdir()
+#
+# os.system('module load intel/19.1.2')
+# os.system('module load openmpi/intel/4.1.1')
 
-os.system('module load intel/19.1.2')
-os.system('module load openmpi/intel/4.1.1')
+shuffle(run_dirs)  # make parallel runs feasible
 
-copy_tree(classifier_source_path, './')
-copyfile('example/weight_nicotinamide.txt','weight.txt')
-copyfile('example/INPUT_nico_cluster2.dat', 'INPUT.dat')
-
-os.system('make')
-
-for dir_path in run_dirs:
+for dir_path in run_dirs[:5]:
     try_iter = False
     finished = False
     wait_steps = 0
@@ -28,16 +25,13 @@ for dir_path in run_dirs:
     except:
         pass
 
-    if try_iter:
-        os.system(f'./sa {dir_path}/traj.dump lammps_vec INPUT.dat')
-        sleep(10)
-
-        while (not finished) or (wait_steps < 20): # wait a maximum of 200 seconds before giving up
-            if os.path.exists('run_ave_traj.dump'):
-                finished = True
-                os.rename('run_ave_traj.dump', dir_path + '/run_ave_traj.dump')
-                os.rename('new_traj.dump', dir_path + '/new_traj.dump')
-            else:
-                sleep(10)
-                wait_steps += 1
+    if try_iter and (not os.path.exists(dir_path + '/run_ave_traj.dump')):
+        copy_tree(classifier_source_path, dir_path)
+        copyfile('example/weight_nicotinamide.txt', dir_path + 'weight.txt')
+        copyfile('example/INPUT_nico_cluster2.dat', dir_path + 'INPUT.dat')
+        os.chdir(dir_path)
+        os.system('make')
+        sleep(20)
+        os.system(f'./sa traj.dump lammps_vec INPUT.dat')
+        os.chdir('../')
 
