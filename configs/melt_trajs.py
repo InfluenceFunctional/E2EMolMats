3164e094f18a028f0c6abda362c6dfb86e4395a0
InfluenceFunctional <mjakilgour@gmail.com>
2023-10-29 17:36:16 -0400

    :> for on-cluster analysis

---
+++ configs/melt_trajs.py
@@ -0,0 +1,28 @@
+'''run configs to loop over - will take all combinations of the below lists - grows combinatorially!!'''
+
+batch_config = {
+    # loop-overable (must be a list)
+    'cluster_size': [[10, 10, 10]],
+    'temperature': [500, 700, 900],
+    'structure_identifier': ["NICOAM13",
+                             "NICOAM16",
+                             "NICOAM17"],
+    'defect_rate': [0],
+    'gap_rate': [0],
+    'scramble_rate': [0],
+    'seed': [1],
+    'damping': [str(100.0)],
+    'max_sphere_radius': [20, 30, 40],
+
+    # static items - DO NOT SET AS LIST
+    'cluster_type': 'supercell',
+    'run_time': int(1e6),
+    'box_type': 'p',
+    'integrator': 'nosehoover',
+    'print_steps': int(1e3),
+    'min_inter_cluster_distance': None,
+    'bulk_crystal': False,
+    'machine': 'cluster',
+    'run_name': 'melt_trajs',
+    'min_lattice_length': 40,
+}
