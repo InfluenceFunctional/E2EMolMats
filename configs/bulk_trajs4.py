a10d6ea6677db2ac559a5939a0f24090ac7f1a6d
InfluenceFunctional <mjakilgour@gmail.com>
2023-11-23 16:01:22 -0500

    :> for on-cluster analysis

---
+++ configs/bulk_trajs4.py
@@ -0,0 +1,34 @@
+'''run configs to loop over - will take all combinations of the below lists - grows combinatorially!!'''
+
+batch_config = {
+    # loop-overable (must be a list)
+    'cluster_size': [[10, 10, 10]],
+    'temperature': [500, 600, 700],
+    'structure_identifier': ["NICOAM07",
+                             "NICOAM08",
+                             "NICOAM09",
+                             "NICOAM13",
+                             "NICOAM14",
+                             "NICOAM15",
+                             "NICOAM16",
+                             "NICOAM17",
+                             "NICOAM18"],
+    'defect_rate': [0],
+    'gap_rate': [0],
+    'scramble_rate': [0],
+    'seed': [1],
+    'damping': [str(100.0)],
+    'max_sphere_radius': [None],
+
+    # static items - DO NOT SET AS LIST
+    'cluster_type': 'supercell',
+    'run_time': int(1e6),
+    'box_type': 'p',
+    'integrator': 'npt',
+    'print_steps': int(1e2),
+    'min_inter_cluster_distance': None,
+    'bulk_crystal': True,
+    'machine': 'cluster',
+    'run_name': 'bulk_trajs4',
+    'min_lattice_length': 40,
+}
