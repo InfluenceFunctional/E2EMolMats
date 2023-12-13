2d8cd8b01fb9fe08c72b0280774433982137d7e8
InfluenceFunctional <mjakilgour@gmail.com>
2023-12-13 17:13:53 -0500

    :> testing functionality for crystal-in-melt

--- configs/melt_cluster_test5.py
+++ configs/melt_cluster_test5.py
@@ -3,14 +3,14 @@
 batch_config = {
     # loop-overable (must be a list)
     'cluster_size': [[10, 10, 10]],
-    'temperature': [100, 200],
+    'temperature': [100, 150],
     'structure_identifier': ["NICOAM13", "NICOAM16", "NICOAM17"],
     'defect_rate': [0],
     'gap_rate': [0],
     'scramble_rate': [0],
     'seed': [2],
     'damping': [str(100.0)],
-    'max_sphere_radius': [15, 20],
+    'max_sphere_radius': [15],
 
     # static items - DO NOT SET AS LIST
     'cluster_type': 'supercell',
@@ -22,7 +22,7 @@ batch_config = {
     'bulk_crystal': True,
     'machine': 'cluster',
     'run_name': 'crystal_in_melt_test5',
-    'min_lattice_length': 80,
+    'min_lattice_length': 60,
     'prep_crystal_in_melt': True,
     'equil_time': int(2e5),
     'melt_temperature': 2000,
