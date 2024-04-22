bd011395f121570084408dc9542abac497d3351d
InfluenceFunctional <mjakilgour@gmail.com>
2024-04-22 13:06:38 -0400

    :> test 5 :> jitter-equilibration

--- e2emolmats/analysis/interface_melt_analysis.py
+++ e2emolmats/analysis/interface_melt_analysis.py
@@ -10,7 +10,7 @@ import wandb
 import glob
 
 battery_paths = [
-    r'D:\crystal_datasets\acridine_melt_interface3/',
+    r'D:\crystal_datasets\acridine_melt_interface4/',
 ]
 
 combined_df = pd.DataFrame()
