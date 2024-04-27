b555cdf0ade68a1443f822dee547ca2fceec18d5
InfluenceFunctional <mjakilgour@gmail.com>
2024-04-27 16:16:54 -0400

    :> new spring equilibration :> test 7

--- configs/experiments/acridine_melt_interfaces/test10.py
+++ configs/experiments/acridine_melt_interfaces/test10.py
@@ -29,8 +29,8 @@ batch_config = {
     'min_inter_cluster_distance': 0,  # 40,  # sets periodic box size in cluster simulations, 0 or None if unused
     'bulk_crystal': True,  # if true, periodic structure
     'machine': 'cluster',  # 'local' or 'cluster' have different associated paths
-    'run_name': 'acridine_melt_interface9',
-    'min_lattice_length': [45],
+    'run_name': 'acridine_melt_interface10',
+    'min_lattice_length': [30],
     # for periodic bulk simulations. Supercell a x b x c a,b,c will be set to approximately at least this edge length.
     'prep_crystal_in_melt': False,  # Work in progress - prepare a frozen nanocrystal in a melted environment
     'prep_melt_interface': True,  # Work in progress - split supercell in half along the fractional z direction
