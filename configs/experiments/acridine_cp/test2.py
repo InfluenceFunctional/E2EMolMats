8837f12cc4c0b41d17b538f0e05872014ee97669
InfluenceFunctional <mjakilgour@gmail.com>
2024-06-29 10:03:50 -0400

    :> update bond creation

--- configs/experiments/acridine_cp/test2.py
+++ configs/experiments/acridine_cp/test2.py
@@ -35,11 +35,11 @@ batch_config = {
     'ramp_temperature': False,  # linearly ramp temperature in main sampling run from 0-temperature
     'init_temperature': 200,  # for ramps only
     'print_steps': int(2e2),  # how many timepoints to print in sampling trajectory
-    'min_inter_cluster_distance': 30,  # sets periodic box size in cluster simulations, 0 or None if unused
+    'min_inter_cluster_distance': 20,  # sets periodic box size in cluster simulations, 0 or None if unused
     'bulk_crystal': True,  # if true, periodic structure
     'machine': 'cluster',  # 'local' or 'cluster' have different associated paths
     'run_name': 'acridine_cp2',
-    'min_lattice_length': [20],
+    'min_lattice_length': [20, 40],
     # for periodic bulk simulations. Supercell a x b x c a,b,c will be set to approximately at least this edge length.
     'prep_crystal_in_melt': False,  # Work in progress - prepare a frozen nanocrystal in a melted environment
     'prep_melt_interface': False,  # Work in progress - split supercell in half along the fractional z direction
