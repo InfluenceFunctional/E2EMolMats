61b564cbaf30ddb4c125ecb577a6c6bd29df0962
InfluenceFunctional <mjakilgour@gmail.com>
2024-08-30 16:31:30 -0400

    :> update bond creation

--- configs/experiments/acridine_melt_interfaces/test1.py
+++ configs/experiments/acridine_melt_interfaces/test1.py
@@ -7,7 +7,8 @@ batch_config = {
     # loop-overable (must be a list)
     'cluster_size': [[10, 10, 10]],  # size of initial bulk supercell, from which finite subsamples may be carved. Should generally be *very large*
     'temperature': [350, 360, 370, 380, 390, 400, 410],  # Kelvin
-    'structure_identifier': ['acridine/Form2', 'acridine/Form4'],
+    'structure_identifier': ['acridine/Form2',
+                             'acridine/Form4'],
     'defect_rate': [0],
     'defect_type': [None],
     # what molecule to substitute in the lattice - 'benzamide' or 'isonicotinamide' for nicotinamide, 'anthracene' or '2,7-dihydroxynaphthalene' for acridine
@@ -16,6 +17,7 @@ batch_config = {
     'seed': [1, 2],  # integers greater than zero
     'damping': [str(100.0)],  # for LAMMPS Langevin dynamics
     'max_sphere_radius': [10],  # if carving a finite cluster from a bulk structure, the radius of the sphere
+    'invert_defects': False,
 
     # static items - DO NOT SET AS LIST
     'run_time': 1e7,  # sampling time in femtoseconds
@@ -23,7 +25,8 @@ batch_config = {
     # type of structure to simulate. "supercell" a nxnxn bulk crystal supercell. "spherical" a finite cluster in vacuum.
     'box_type': 'p',
     # box type in LAMMPS dimensions 'p' for periodic typically used even for vacuum simulations, just with very large box
-    'integrator': 'npzt',  # nosehoover, npt, nvt
+    'integrator': 'npt',  # nosehoover, npt, nvt
+    'pressure_direction': ['x', 'y', 'z'],  # iso, x, y, or z for npt pressure directionality
     'ramp_temperature': False,  # linearly ramp temperature in main sampling run from 0-temperature
     'init_temperature': 200,  # for ramps only
     'print_steps': int(1e2),  # how many timepoints to print in sampling trajectory
